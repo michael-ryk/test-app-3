40313fd74c52b99365ea486bf119c1bc02b4eb1e
MichaelR <spider747@gmail.com>
2021-11-30 23:12:22 +0200

    Split components

---
+++ src/Book.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+// Component Book
+const Book = ({author, imgLink, title}) => {
+
+    //When button clicked - simple event
+    const clickHandler = () => {
+      console.log('Click Handler: button clicked');
+    }
+  
+    //When button clicked - pass parameter
+    const complexClickHandler = (e) =>{
+      console.log(e);
+      console.log(e.target);
+    }
+  
+    return (
+      <article className='book'>
+        <img src={imgLink} alt='' />
+        <h1 onClick={() => console.log("Title: " + title)}>{title}</h1>
+        <h4>{author}</h4>
+        <button type="button" onClick={clickHandler}>Button</button>
+        <button type="button" onClick={complexClickHandler}>Complex Event</button>
+      </article>
+    );
+  }
+
+export default Book
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import books from './books';
+import Book from './Book';
 
 // Import CSS
 import './index.css'
@@ -17,18 +18,5 @@ function BookList(){
     </section>
   );
 }
-
-// Component Book
-const Book = ({author, imgLink, title}) => {
-  return (
-    <article className='book'>
-      <img src={imgLink} alt='' />
-      <h1>{title}</h1>
-      <h4>{author}</h4>
-    </article>
-  );
-}
-
-//Must be expression and cannot be a statement
 
 ReactDom.render(<BookList/>,document.getElementById('root'));
